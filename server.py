c3652c0c9a8f8c99300b78c0805db833a13abf25
Your Name <you@example.com>
2020-01-22 13:02:21 -0800

    finish server compliment and insult function

--- server.py
+++ server.py
@@ -13,13 +13,26 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+INSULTS = [
+    'bad', 'strange', 'a bad programmer', 'evil', 'a witch']
+
 
 @app.route("/")
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
-
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Home Page</title>
+      </head>
+      <body>
+        <h1>Hi! This is the home page.</h1>
+        <a href="http://localhost:5000/hello">Hello</a>
+      </body>
+    </html>
+    """
 
 @app.route("/hello")
 def say_hello():
@@ -35,12 +48,43 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
+          <br>
+          Choose compliment: <select name="compliment">
+            <option value="awesome">Awesome</option>
+            <option value="fantastic">Fantastic</option>
+            <option value="cool">Cool</option></select>
+            <br>
+          <input type="submit" value="Compliment Me">
+        </form>
+        <br>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <br>
+          <input type="submit" value="Insult Me">
         </form>
       </body>
     </html>
     """
 
+@app.route("/diss")
+def insult_person():
+    """Insult user by name."""
+
+    player = request.args.get("person")
+
+    insult = choice(INSULTS)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>An Insult</title>
+      </head>
+      <body>
+        Hi, inferior {player}! I think you're {insult}!
+      </body>
+    </html>
+    """
 
 @app.route("/greet")
 def greet_person():
@@ -48,9 +92,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
-    y = x
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
@@ -68,4 +110,4 @@ def greet_person():
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
